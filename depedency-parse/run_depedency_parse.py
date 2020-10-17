e98fc4b16d089d251f3e43eaf636f7aae8a0c9da
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2020-10-17 20:31:51 +0700

    add print

--- depedency-parse/run_depedency_parse.py
+++ depedency-parse/run_depedency_parse.py
@@ -1,7 +1,7 @@
 import stanza
 
-list_image_name = open('data/task1/image_splits/all_images.txt','r').read().split('\n')
-list_caption = open('data/task1/raw/en_labels/all_label_en.txt','r').read().split('\n') 
+list_image_name = open('../data/task1/image_splits/all_images.txt','r').read().split('\n')
+list_caption = open('../data/task1/raw/en_labels/all_label_en.txt','r').read().split('\n') 
 
 total_images = len(list_image_name)
 
@@ -11,6 +11,8 @@ for i in range len(total_images):
   image_name = list_image_name[i]
   caption = list_caption[i]
   doc = nlp(caption)
+  
+  print(f'{i}: {image_name}')
   
   target_file =  open(f'en/{image_name[:-4]}.txt', 'w')
   target_file.write(*[f'id: {word.id}\tword: {word.text}\thead id: {word.head}\thead: {sent.words[word.head-1].text if word.head > 0 else "root"}\tdeprel: {word.deprel}' for sent in doc.sentences for word in sent.words], sep='\n')
